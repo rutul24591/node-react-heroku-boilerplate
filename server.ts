c98e1fffaa6eebf6ed94044ff46e0abb10c39f4a
Rutul Amin <rutulamin.ra@gmail.com>
2022-04-22 17:49:22 +0530

    Heroku

--- server.ts
+++ server.ts
@@ -1,10 +1,24 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
 app.get("/", (_req, res) => {
   res.send("Hello World!");
+});
+
+// Handle GET requests to /api route
+app.get("/api", (_req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (_req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(port, () => {
